5e31992c77535bdf9f8b0406cb70fa5f39248b20
ajeje93 <serran93@hotmail.it>
2021-12-29 01:00:49 +0100

    ci: :green_heart: fix ci env variable for vercel

--- apps/client/scripts/prebuild.js
+++ apps/client/scripts/prebuild.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-if (process.env.CI === "true") {
+if (process.env.CI === "1") {
   const reactAppEnvs = Object.fromEntries(
     Object.entries(process.env).filter((i) => i[0].startsWith("REACT_APP_"))
   );
